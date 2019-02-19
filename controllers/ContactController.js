64db5765d82f959fad4163413eff4213285ef70e
eghc <eringraceharris@gmail.com>
2019-02-18 17:26:30 -0800

    database checkpoint complete

---
+++ controllers/ContactController.js
@@ -0,0 +1,34 @@
+const inquirer = require("inquirer");
+const Contact = require("../db/models").Contact;
+
+module.exports = class ContactController {
+
+  constructor(){
+    this.contacts = [];
+    this.addContactQuestions = [
+       {
+         type: "input",
+         name: "name",
+         message: "Contact's name - ",
+         validate(val){
+           return val !== "";
+         }
+       },
+       {
+         type: "input",
+         name: "phone",
+         message: "Contact's phone number - ",
+         validate(val){
+           return val !== "";
+         }
+       }
+     ];
+  }
+
+  addContact(name, phone){
+    console.log("done");
+     return Contact.create({name, phone});
+     console.log("done2");
+   }
+
+}
